c36eca74dad2a3d769849c4c9d8199f3287fde2e
IsabellaAlves <isabellachoucatealves@hotmail.com>
2015-12-04 14:12:49 +1100

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+// Database URL
+module.exports = {
+    // Uncomment to connect with MongoDB on Cloud
+    //'url' : 'mongodb://feiochc:hate666!@kahana.mongohq.com:10073/node-api'
+   'url': mongodb://03-express-ica-template-ica:$$$03-nuvem-express-***@ds054308.mongolab.com:54308/03-express-ica-template
+
+    //'url' : 'localhost/03-express-ica-template'
+};
